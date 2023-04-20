11f6886eae8c4ce76285bb498faaf6e54511465a
李大毛 <dev@darmau.design>
2023-04-20 17:41:58 +0800

    update umami setting

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,8 @@ const Layout = ({ children }) => {
         async
         src="https://analytics.darmau.design/script.js"
         data-website-id="12e6b15e-9ced-46d4-8b7f-45adb3c653f0"
+        data-do-not-track="true"
+        data-cache="true"
       />
       <div className="bg-white">
         <Header />
